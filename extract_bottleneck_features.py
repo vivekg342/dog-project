de94afb3e95c1d5479a83991b6ef27181cf0e17a
Vivek <vivekg342@gmail.com>
2018-11-27 17:54:53 +0000

    Finish implementation

--- extract_bottleneck_features.py
+++ extract_bottleneck_features.py
@@ -8,7 +8,7 @@ def extract_VGG19(tensor):
 
 def extract_Resnet50(tensor):
 	from keras.applications.resnet50 import ResNet50, preprocess_input
-	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+	return ResNet50(weights='imagenet', input_tensor=Input(shape=(224,224,3), include_top=False).predict(preprocess_input(tensor))
 
 def extract_Xception(tensor):
 	from keras.applications.xception import Xception, preprocess_input
